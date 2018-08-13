d354e54724cc3dddd77d2bc736b16ed4364f9511
David Major <djmajor@princeton.edu>
2018-08-12 20:42:28 -0400

    Website final #1

--- resume_page/views.py
+++ resume_page/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Since this website is just to display the main page, there's just one view
+def homepage(request):
+    return render(request, 'resume_page/homepage.html', {})
